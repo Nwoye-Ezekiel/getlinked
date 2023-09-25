6e829aef00107c9a40c046b6d042270a9f8ae8ff
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2023-09-25 14:38:34 +0100

    ui changes

--- src/components/pages/app/home/Timeline.tsx
+++ src/components/pages/app/home/Timeline.tsx
@@ -44,16 +44,16 @@ const Timeline = () => {
   return (
     <div id="timeline" className="border-t border-t-solid border-t-white/[0.18] lg:pt-6 lg:pb-10">
       <div className="m-12">
-        <div className="max-w-tablet lg:max-w-6xl mx-auto flex flex-col justify-center space-y-14 lg:space-y-40">
+        <div className="max-w-tablet lg:max-w-6xl mx-auto flex flex-col justify-center space-y-10 lg:space-y-32">
           <div className="flex flex-col justify-center items-center text-center space-y-3 lg:space-y-4">
             <h1 className="text-mdMd lg:text-lgMd">Timeline</h1>
             <p className="text-smMd lg:text-base max-w-[40ch] leading-loose">
               Here is the breakdown of the time we anticipate using for the upcoming event.
             </p>
           </div>
-          <div className="relative flex flex-col lg:items-center space-y-5 lg:space-y-14">
+          <div className="relative flex flex-col lg:items-center space-y-3 lg:space-y-12">
             {isDesktop && (
-              <div className="absolute -top-16 left-1/2 transform -translate-x-1/2 h-full w-1 bg-primary"></div>
+              <div className="absolute -top-10 left-1/2 transform -translate-x-1/2 h-[calc(100%-50px)] w-1 bg-primary"></div>
             )}
             {timelines.map((timeline, index) => (
               <div key={index} className="z-10">
@@ -76,26 +76,26 @@ const Timeline = () => {
                     {(index + 1) % 2 !== 0 ? (
                       <>
                         <div className="space-y-2 text-right w-1/2">
-                          <p className="text-primary text-mdMax font-bold">{timeline.title}</p>
+                          <p className="text-primary text-mdMd font-bold">{timeline.title}</p>
                           <p className="max-w-[70ch] leading-loose">{timeline.description}</p>
                         </div>
                         <div className="w-[76px] h-[76px] rounded-full bg-horizontalGradient border-[12px] border-solid border-background flex justify-center items-center text-mdMax font-bold flex-shrink-0">
                           {index + 1}
                         </div>
-                        <div className="text-primary text-mdMax font-bold w-1/2">
+                        <div className="text-primary text-mdMd font-bold w-1/2">
                           {timeline.date}
                         </div>
                       </>
                     ) : (
                       <>
-                        <div className="text-primary text-mdMax font-bold w-1/2 text-right">
+                        <div className="text-primary text-mdMd font-bold w-1/2 text-right">
                           {timeline.date}
                         </div>
                         <div className="w-[76px] h-[76px] rounded-full bg-horizontalGradient border-[12px] border-solid border-background flex justify-center items-center text-mdMax font-bold flex-shrink-0">
                           {index + 1}
                         </div>
                         <div className="space-y-2 w-1/2">
-                          <p className="text-primary text-mdMax font-bold">{timeline.title}</p>
+                          <p className="text-primary text-mdMd font-bold">{timeline.title}</p>
                           <p className="max-w-[70ch] leading-loose">{timeline.description}</p>
                         </div>
                       </>
