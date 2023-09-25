8013c8593990489995fe32df9ef3197e29be249e
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2023-09-25 12:03:28 +0100

    organize codebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'styles/index.css';
-import theme from 'theme';
 import App from 'App';
+import React from 'react';
+import theme from 'theme';
+import 'styles/index.css';
+import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
