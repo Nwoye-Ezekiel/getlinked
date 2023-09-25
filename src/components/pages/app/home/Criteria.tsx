99298be6840765b439e7d70d566af403e83de40e
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2023-09-25 12:46:50 +0100

    organize codebase

--- src/components/pages/app/home/Criteria.tsx
+++ src/components/pages/app/home/Criteria.tsx
@@ -4,27 +4,27 @@ import WomanHoldingChart from 'assets/images/woman-holding-chart.png';
 const Criteria = () => {
   const criteria = [
     {
-      title: 'Innovation and Creativity:',
+      title: 'Innovation and Creativity',
       description:
         'Evaluate the uniqueness and creativity of the solution. Consider whether it addresses a real-world problem in a novel way or introduces innovative features.',
     },
     {
-      title: 'Functionality:',
+      title: 'Functionality',
       description:
         'Assess how well the solution works. Does it perform its intended functions effectively and without major issues? Judges would consider the completeness and robustness of the solution.',
     },
     {
-      title: 'Impact and Relevance:',
+      title: 'Impact and Relevance',
       description:
         'Determine the potential impact of the solution in the real world. Does it address a significant problem, and is it relevant to the target audience? Judges would assess the potential social, economic, or environmental benefits.',
     },
     {
-      title: 'Technical Complexity:',
+      title: 'Technical Complexity',
       description:
         'Evaluate the technical sophistication of the solution. Judges would consider the complexity of the code, the use of advanced technologies or algorithms, and the scalability of the solution.',
     },
     {
-      title: 'Adherence to Hackathon Rules:',
+      title: 'Adherence to Hackathon Rules',
       description:
         'Judges will Ensure that the team adhered to the rules and guidelines of the hackathon, including deadlines, use of specific technologies or APIs, and any other competition-specific requirements.',
     },
@@ -52,7 +52,7 @@ const Criteria = () => {
                   <div key={index}>
                     <p>
                       <span className="text-pink font-bold text-smMax lg:text-base">
-                        {item.title}
+                        {item.title}:
                       </span>{' '}
                       {item.description}
                     </p>
