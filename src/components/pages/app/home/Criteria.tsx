8a3fef4468a25f01316f2c6a3cea366ec1f3eee2
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2023-09-25 12:45:33 +0100

    organize codebase

--- src/components/pages/app/home/Criteria.tsx
+++ src/components/pages/app/home/Criteria.tsx
@@ -1,7 +1,35 @@
-import WomanHoldingChart from 'assets/images/woman-holding-chart.png';
 import Button from 'components/button';
+import WomanHoldingChart from 'assets/images/woman-holding-chart.png';
 
 const Criteria = () => {
+  const criteria = [
+    {
+      title: 'Innovation and Creativity:',
+      description:
+        'Evaluate the uniqueness and creativity of the solution. Consider whether it addresses a real-world problem in a novel way or introduces innovative features.',
+    },
+    {
+      title: 'Functionality:',
+      description:
+        'Assess how well the solution works. Does it perform its intended functions effectively and without major issues? Judges would consider the completeness and robustness of the solution.',
+    },
+    {
+      title: 'Impact and Relevance:',
+      description:
+        'Determine the potential impact of the solution in the real world. Does it address a significant problem, and is it relevant to the target audience? Judges would assess the potential social, economic, or environmental benefits.',
+    },
+    {
+      title: 'Technical Complexity:',
+      description:
+        'Evaluate the technical sophistication of the solution. Judges would consider the complexity of the code, the use of advanced technologies or algorithms, and the scalability of the solution.',
+    },
+    {
+      title: 'Adherence to Hackathon Rules:',
+      description:
+        'Judges will Ensure that the team adhered to the rules and guidelines of the hackathon, including deadlines, use of specific technologies or APIs, and any other competition-specific requirements.',
+    },
+  ];
+
   return (
     <div className="border-t border-t-solid border-t-white/[0.18] lg:pt-8 lg:pb-20">
       <div className="m-12 lg:mb-8">
@@ -20,45 +48,16 @@ const Criteria = () => {
                 <h1 className="text-primary">Key attributes</h1>
               </div>
               <div className="flex flex-col space-y-5 leading-relaxed text-xs lg:text-smMd">
-                <p>
-                  <span className="text-pink font-bold text-smMax lg:text-base">
-                    Innovation and Creativity:
-                  </span>{' '}
-                  Evaluate the uniqueness and creativity of the solution. Consider whether it
-                  addresses a real-world problem in a novel way or introduces innovative features.
-                </p>
-                <p>
-                  <span className="text-pink font-bold text-smMax lg:text-base">
-                    Functionality:
-                  </span>{' '}
-                  Assess how well the solution works. Does it perform its intended functions
-                  effectively and without major issues? Judges would consider the completeness and
-                  robustness of the solution.
-                </p>
-                <p>
-                  <span className="text-pink font-bold text-smMax lg:text-base">
-                    Impact and Relevance:
-                  </span>{' '}
-                  Determine the potential impact of the solution in the real world. Does it address
-                  a significant problem, and is it relevant to the target audience? Judges would
-                  assess the potential social, economic, or environmental benefits.
-                </p>
-                <p>
-                  <span className="text-pink font-bold text-smMax lg:text-base">
-                    Technical Complexity:
-                  </span>{' '}
-                  Evaluate the technical sophistication of the solution. Judges would consider the
-                  complexity of the code, the use of advanced technologies or algorithms, and the
-                  scalability of the solution.
-                </p>
-                <p>
-                  <span className="text-pink font-bold text-smMax lg:text-base">
-                    Adherence to Hackathon Rules:
-                  </span>{' '}
-                  Judges will Ensure that the team adhered to the rules and guidelines of the
-                  hackathon, including deadlines, use of specific technologies or APIs, and any
-                  other competition-specific requirements.
-                </p>
+                {criteria.map((item, index) => (
+                  <div key={index}>
+                    <p>
+                      <span className="text-pink font-bold text-smMax lg:text-base">
+                        {item.title}
+                      </span>{' '}
+                      {item.description}
+                    </p>
+                  </div>
+                ))}
               </div>
             </div>
             <Button>Read More</Button>
