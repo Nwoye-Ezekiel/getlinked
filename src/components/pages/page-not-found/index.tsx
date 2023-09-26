c7a42a1aea5df156487db5c07ba06e0c58d2b37a
Nwoye Ezekiel <chinecherem.nwoye.cn@gmail.com>
2023-09-26 16:23:52 +0100

    add page not found

--- src/components/pages/page-not-found/index.tsx
+++ src/components/pages/page-not-found/index.tsx
@@ -3,9 +3,9 @@ import { Player } from '@lottiefiles/react-lottie-player';
 
 const PageNotFound = () => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen -mt-[68px] lg:-mt-[125px]">
+    <div className="flex flex-col justify-center items-center h-screen -mt-[100px] lg:-mt-[125px]">
       <Player src={animations['alert']} className="-mb-3 -mt-16 w-72" autoplay loop />
-      <h1 className="text-lg">Page Not Found!</h1>
+      <h1 className="text-mdMax lg:text-lg">Page Not Found!</h1>
     </div>
   );
 };
